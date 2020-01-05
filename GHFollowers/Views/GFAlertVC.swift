c915d540408d7014934c0d80900e9f8f91984721
Taylor Maxwell <taylor@imaxwell.net>
2020-01-05 15:48:21 -0800

    Organization changes
    Moved files into folders

--- GHFollowers/Views/GFAlertVC.swift
+++ GHFollowers/Views/GFAlertVC.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class GFAlertVC: UIViewController {
 
-    let containerView = UIView()
+    let containerView = GFContainerView()
     let titleLabel = GFTitleLabel(textAlignment: .center, fontSize: 20)
     let messageLabel = GFBodyLabel(textAlignment: .center)
     let actionButton = GFButton(backgroundColor: .systemPink, title: "Ok")
@@ -44,11 +44,6 @@ class GFAlertVC: UIViewController {
     
     func configureContainerView() {
         view.addSubview(containerView)
-        containerView.backgroundColor = .systemBackground
-        containerView.layer.cornerRadius = 16
-        containerView.layer.borderWidth = 2
-        containerView.layer.borderColor = UIColor.white.cgColor
-        containerView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             containerView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
---
+++ GHFollowers/Views/GFContainerView.swift
@@ -0,0 +1,31 @@
+//
+//  GFContainerView.swift
+//  GHFollowers
+//
+//  Created by Taylor Maxwell on 1/5/20.
+//  Copyright © 2020 Taylor Maxwell. All rights reserved.
+//
+
+import UIKit
+
+class GFContainerView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        
+        backgroundColor = .systemBackground
+        layer.cornerRadius = 16
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.white.cgColor
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+
+}
